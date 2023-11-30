b74cf81a4d65bf4795b8cf5f32c20da25c0f456a
alexandre-pod <24512899+alexandre-pod@users.noreply.github.com>
2023-11-30 23:49:49 +0100

    Prepare 2023

---
+++ 2023/_00/Sources/Main/main.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+typealias InputType = [String]
+
+func main() {
+    var input: InputType = []
+
+    while let line = readLine() {
+        input.append(line)
+    }
+
+    print(part1(inputParameter: input))
+    print(part2(inputParameter: input))
+}
+
+// MARK: - Part 1
+
+func part1(inputParameter: InputType) -> Int {
+    fatalError()
+}
+
+// MARK: - Part 2
+
+func part2(inputParameter: InputType) -> Int {
+    fatalError()
+}
+
+main()
